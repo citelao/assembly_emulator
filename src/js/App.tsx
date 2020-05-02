c418cc5b3fb8b826e03e7ba2e57ebec6c0f7ad2e
Ben <ben@stolovitz.com>
2020-05-02 11:48:29 -0400

    looks like an emulator

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+interface IEmulatorState {
+    ra: number;
+    rb: number;
+    rc: number;
+}
+
 interface IAppProps {
     code: string[];
+    emulatorState: IEmulatorState[];
 }
 
 class App extends React.Component<IAppProps, {}> {
@@ -10,21 +17,41 @@ class App extends React.Component<IAppProps, {}> {
         super(props);
     }
 
+    private renderRow(index: number): JSX.Element {
+        const line = (this.props.code.length <= index)
+            ? ""
+            : this.props.code[index];
+        const emulator = this.props.emulatorState[index];
+        return <tr>
+            <td><code>{line}</code></td>
+            <td>{emulator.ra}</td>
+            <td>{emulator.rb}</td>
+            <td>{emulator.rc}</td>
+        </tr>;
+    }
+
     render() {
         return <main>
-            <pre>
-                {this.props.code.map(line => <>{line + "\r\n"}</>)}
-            </pre>
+            <table>
+                <tbody>
+                   {this.props.emulatorState.map((_, index) => this.renderRow(index))}
+                </tbody>
+            </table>
         </main>;
     }
 }
 
 const props: IAppProps = {
     code: [
-        "foo",
-        "bar"
+        "set rb to 240",
+        "set rc to 12",
+    ],
+    emulatorState: [
+        { ra: 0, rb: 0, rc: 0 },
+        { ra: 0, rb: 240, rc: 0 },
+        { ra: 0, rb: 240, rc: 12 },
     ]
 };
 
 const mountNode = document.getElementById("app");
-ReactDOM.render(<App code={props.code} />, mountNode);
+ReactDOM.render(<App code={props.code} emulatorState={props.emulatorState} />, mountNode);
