a96d5fe28a20391719945cdf4133aa3951cb8df5
Ben <ben@stolovitz.com>
2020-05-02 13:03:14 -0400

    refactor and add jump

--- src/js/emulator/Emulator.ts
+++ src/js/emulator/Emulator.ts
@@ -4,18 +4,32 @@ export default class Emulator {
     public static run(oldState: IEmulatorState, command: EmulatorCommand): IEmulatorState {
         if (command === "nop") {
             return oldState;
-        } else if (command.command === "set") {
-            const partialState: Partial<IEmulatorState> = {};
-            partialState[command.register] = command.value;
-            return Object.assign({}, oldState, partialState);
-        } else if (command.command === "increment") {
-            const partialState: Partial<IEmulatorState> = {};
-            partialState[command.register] = oldState[command.register] + command.by;
-            return Object.assign({}, oldState, partialState);
-        } else if (command.command === "add") {
-            const partialState: Partial<IEmulatorState> = {};
-            partialState[command.to] = oldState[command.a] + oldState[command.b];
-            return Object.assign({}, oldState, partialState);
+        } 
+        
+        switch(command.command) {
+            case "set":
+            {
+                const partialState: Partial<IEmulatorState> = {};
+                partialState[command.register] = command.value;
+                return Object.assign({}, oldState, partialState);
+            }
+            case "increment":
+            {
+                const partialState: Partial<IEmulatorState> = {};
+                partialState[command.register] = oldState[command.register] + command.by;
+                return Object.assign({}, oldState, partialState);
+            }
+            case "add":
+            {
+                const partialState: Partial<IEmulatorState> = {};
+                partialState[command.to] = oldState[command.a] + oldState[command.b];
+                return Object.assign({}, oldState, partialState);
+            }
+            case "jump":
+            {
+                // uhoh.
+                throw new Error("uh oh");
+            }
         }
     }
 }
--- src/js/emulator/EmulatorTypes.ts
+++ src/js/emulator/EmulatorTypes.ts
@@ -1,8 +1,8 @@
 export const REGISTER_MAX = 256;
 export type RegisterValue = number;
+export type Address = number;
 
 export type RegisterName = "ra" | "rb" | "rc";
-
 export interface IEmulatorState {
     ra: RegisterValue;
     rb: RegisterValue;
@@ -26,16 +26,25 @@ export type EmulatorCommand =
         to: RegisterName,
         a: RegisterName,
         b: RegisterName
+    } |
+    {
+        command: "jump",
+        to: Address
     };
 
 export function stringify(command: EmulatorCommand): string {
     if (command === "nop") {
         return command;
-    } else if (command.command === "set") {
-        return `set ${command.register} to ${command.value}`;
-    } else if (command.command === "increment") {
-        return `increment ${command.register} by ${command.by}`;
-    } else if (command.command === "add") {
-        return `${command.to} = ${command.a} + ${command.b}`;
+    }
+    
+    switch(command.command) {
+        case "set":
+            return `set ${command.register} to ${command.value}`;
+        case "increment":
+            return `increment ${command.register} by ${command.by}`;
+        case "add":
+            return `${command.to} = ${command.a} + ${command.b}`;
+        case "jump":
+            return `jump to ${command.to}`;
     }
 }
